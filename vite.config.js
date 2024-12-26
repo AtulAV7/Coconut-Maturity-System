65c26b7c51468caf3c8728c84ea5201646f36d3b
Klaytn <122271792+AtulAV7@users.noreply.github.com>
2024-12-26 22:29:11 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+  build: {
+    rollupOptions: {
+      external: ['/src/main.jsx'], // Externalize the main.jsx file
     },
-  }
-})
+  },
+});
